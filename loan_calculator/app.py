93b7a0ff263344ef49e4fe450009c334ad7ee2b2
Edmund Lee <edmundlhs1104@gmail.com>
2024-01-16 19:15:01 +0800

    update run

--- loan_calculator/app.py
+++ loan_calculator/app.py
@@ -91,6 +91,6 @@ server = app.server
 
 if __name__ == "__main__":
     app.layout = create_layout()
-    app.run(debug=False, host="0.0.0.0",port=8080)
+    app.run_server(debug=False, host="0.0.0.0",port=8080)
 
 
